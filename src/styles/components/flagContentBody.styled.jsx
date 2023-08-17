dfe0a63561d9d56916eb9210c0cf14965cfa318f
Henry Jacobs <henryjacobscode@gmail.com>
2023-08-17 13:24:02 -0400

    styles for displaycontentheader and body components

---
+++ src/styles/components/flagContentBody.styled.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+const FlagContentBodyStyled = styled.section`
+    width: 100%;
+    padding: 32px;
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+`
+
+export default FlagContentBodyStyled
