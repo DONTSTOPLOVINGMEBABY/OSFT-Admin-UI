9d40a1cea80476cb3562ee4ac4bcd87e2a673700
Henry Jacobs <henryjacobscode@gmail.com>
2023-08-24 11:51:33 -0400

    turn project drop down into its own component

---
+++ src/forms/projectDropDown.jsx
@@ -0,0 +1,43 @@
+// move project drop down to its own comp 
+import Select from "react-select"
+import loaders from "../loaders"
+import { useQuery } from "react-query"
+import { useEffect, useState } from "react"
+
+function ProjectDropDown ({
+    setProjectDropDownValue
+}) {
+
+    const [allProjects, setAllProjects] = useState([])
+    const { data, isLoading, error } = useQuery(['projects'], loaders.projects.ProjectLoader)
+
+    useEffect(() => {
+        if (data){
+            let options = []
+            data.names.forEach( name => {
+                options.push({
+                    value: name, 
+                    label : name, 
+                })
+            })
+            setAllProjects(options)
+        }
+    }, [data])
+
+    return (
+        <Select 
+            options={allProjects}
+            placeholder={"Pick a Project"}
+            styles={{
+                container : (baseStyles, state) => ({
+                    ...baseStyles, 
+                    width : '100%'
+                })
+            }}
+            onChange={(selectedOption) => setProjectDropDownValue(selectedOption.value)}
+        />
+    )
+
+}
+
+export default ProjectDropDown
