8c36c9aba65df36867c098bcb9edc372cfa89262
Hankus Pankus <henryjacobscode@gmail.com>
2023-08-25 20:52:54 -0400

    Make footer smaller

--- src/styles/components/footer/footer.styled.jsx
+++ src/styles/components/footer/footer.styled.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components"
 const FooterStyled = styled.footer`
     flex-grow: 1 ; 
     background-color : ${ props => props.theme.background.content } ; 
-    min-height: 100px;
+    min-height: 50px;
 `
 
 export { FooterStyled }
